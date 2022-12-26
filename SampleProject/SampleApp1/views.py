43c2bd3dd035435270a8fddfacf70fb36d7974db
jeevan6302 <jeevannayak209@gmail.com>
2022-12-27 00:23:01 +0530

    firstcommit

---
+++ SampleProject/SampleApp1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render;
+from django.http import HttpResponse
+
+#Create your views here...
+def f11(request): 
+	return HttpResponse("<h1>Hello from SampleApp1 f11()</h1><hr />"); 
+def f22(request): 
+	return HttpResponse("<h1>Hello from SampleApp1 f22()</h1><hr />"); 
+
+
+
+def homepage(request):
+    return HttpResponse("<h1>Hello from SampleProject Homepage...!!!</h1><hr />");
